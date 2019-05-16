1df1204d4fa733962fa96300aaac97db48fae12b
matthew-laine <matthew.i.laine@gmail.com>
2019-05-16 10:18:48 -0400

    create virtualPet and give it name and hunger

---
+++ src/main/java/VirtualPet.java
@@ -0,0 +1,24 @@
+
+public class VirtualPet {
+	
+	String name;
+	int hunger = 10;
+	
+	public VirtualPet() {
+
+	}
+
+	public VirtualPet(String petName) {
+		name = petName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getHunger() {
+		// TODO Auto-generated method stub
+		return hunger;
+	}
+
+}
