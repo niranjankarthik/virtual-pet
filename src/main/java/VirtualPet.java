e55ab5f19660aebe6194dd8cdf6e050a94586660
Sbsmith1101 <sbsmith1101@gmail.com>
2019-05-16 14:35:57 -0400

    previous status

---
+++ src/main/java/VirtualPet.java
@@ -0,0 +1,21 @@
+
+public class VirtualPet {
+	String name;
+	int hunger = 10;
+
+	public VirtualPet() {
+	}
+	public VirtualPet(String petName) {
+		name = petName;
+
+	}
+	public String getName() {
+		return name;
+	}
+
+	public int getHunger() {
+		// TODO Auto-generated method stub
+		return hunger;
+	}
+
+}
