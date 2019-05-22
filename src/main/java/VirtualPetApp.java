62ece302cc101ec841601be375dd2083cab66d0e
Sbsmith1101 <sbsmith1101@gmail.com>
2019-05-22 09:58:09 -0400

    before function

--- src/main/java/VirtualPetApp.java
+++ src/main/java/VirtualPetApp.java
@@ -7,7 +7,7 @@ public class VirtualPetApp {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please enter a Pet Name");
 		String name = input.nextLine();
-		VirtualPet virtualPet1 = new VirtualPet(name);
+		VirtualPet virtualPet1 = new VirtualPet(name, "");
 		System.out.println("Your Pet is " + name);
 	}
 
