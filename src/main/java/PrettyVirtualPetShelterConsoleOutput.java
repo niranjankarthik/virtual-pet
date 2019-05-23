f0d742cfc47c65c7f1ac3d914e1b87c487b80cba
matthew-laine <matthew.i.laine@gmail.com>
2019-05-23 14:17:53 -0400

    can now print all lines of pet data in shelter

--- src/main/java/PrettyVirtualPetShelterConsoleOutput.java
+++ src/main/java/PrettyVirtualPetShelterConsoleOutput.java
@@ -4,26 +4,21 @@ public class PrettyVirtualPetShelterConsoleOutput {
 
 	public static void main(String[] args) {
 		VirtualPetShelter myShelter = new VirtualPetShelter();
-		
+
 		myShelter.addPetToShelter(new VirtualPet("Sam the Firehouse Cat", "Generic Cat"));
 		myShelter.addPetToShelter(new VirtualPet("Lawrence", "Sane Dog"));
 		myShelter.addPetToShelter(new VirtualPet("Matt", "Crazy Dog"));
 		myShelter.addPetToShelter(new VirtualPet("Isaac", "Howler Monkey"));
 		myShelter.addPetToShelter(new VirtualPet("Joe", "Systematic Dog"));
 		myShelter.addPetToShelter(new VirtualPet("Scott", "Smart Dog"));
-		
-		printPetAttributes(myShelter, "Sam the Firehouse Cat");
-		printPetAttributes(myShelter, "Lawrence");
-//		
-//		System.out.println();
-//		
-//		for(int i = 0; i < myShelter.getAllAttributes().size(); i++) {
-//			for (int j = 0; j < myShelter.getAllAttributes().get(i).size(); j++) {
-//				System.out.printf("|--%s--", myShelter.getAllAttributes().get(i).get(j));
-//			}
-//			System.out.println("|");
-//		}
-		
+
+		printPetAttributesHeader();
+		printSinglePetAttributes(myShelter, "Sam the Firehouse Cat");
+		printSinglePetAttributes(myShelter, "Lawrence");
+		System.out.println();
+		printPetAttributes(myShelter, "Isaac");
+		System.out.println();
+		printAllPetAttributes(myShelter);
 	}
 	private static String make10CharacterStringWithDashes(String input) {
 		input.trim();
@@ -39,18 +34,32 @@ public class PrettyVirtualPetShelterConsoleOutput {
 		}
 		return input;
 	}
-	private static void printPetAttributes(VirtualPetShelter virtualPetShelter, String name) {
+	private static void printSinglePetAttributes(VirtualPetShelter virtualPetShelter, String name) {
 		for(int i = 0; i < virtualPetShelter.getPetAttributes(name).size(); i++) {
 			System.out.printf("|%s", make10CharacterStringWithDashes(virtualPetShelter.getPetAttributes(name).get(i)));
 		}
 		System.out.println("|");
 	}
-	
-//	private static void printAllAttributes(VirtualPetShelter virtualPetShelter) {
-//		String name;
-//		for( : ) {
-//			name = virtualPetShelter.getPetAttributes(name).get(name);
-//			printPetAttributes(virtualPetShelter, name);
-//		}
-//	}
+	private static void printPetAttributesHeader() {
+		System.out.printf("|%s", make10CharacterStringWithDashes("Name"));
+		System.out.printf("|%s", make10CharacterStringWithDashes("Species"));
+		System.out.printf("|%s", make10CharacterStringWithDashes("Hunger"));
+		System.out.printf("|%s", make10CharacterStringWithDashes("Boredom"));
+		System.out.printf("|%s", make10CharacterStringWithDashes("Thirst"));
+		System.out.printf("|%s", make10CharacterStringWithDashes("BoredomCounter"));
+		System.out.printf("|%s", make10CharacterStringWithDashes("Ownership"));
+		System.out.println("|");
+	}
+	private static void printPetAttributes(VirtualPetShelter virtualPetShelter, String name) {
+		printPetAttributesHeader();
+		printSinglePetAttributes(virtualPetShelter, name);
+	}
+	private static void printAllPetAttributes(VirtualPetShelter virtualPetShelter) {
+		printPetAttributesHeader();
+		ArrayList<ArrayList<String>> allAttributes = virtualPetShelter.getAllAttributes();
+		
+		for(int i = 0; i < allAttributes.size(); i++) {
+			printSinglePetAttributes(virtualPetShelter, allAttributes.get(i).get(0));
+		}
+	}
 }
