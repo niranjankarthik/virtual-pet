47982565185100985c4915266f18c1a6a21e7be3
Isaacjk614 <isaac@Isaacs-MacBook-Pro.local>
2019-05-28 11:25:07 -0400

    can print out only common pet attributes of single and all pets

--- src/main/java/ConsoleOutput.java
+++ src/main/java/ConsoleOutput.java
@@ -2,6 +2,14 @@ import java.util.ArrayList;
 
 public class ConsoleOutput {
 
+	public static void main(String[] args) {
+		VirtualPetShelter myShelter = new VirtualPetShelter();
+		myShelter.addPetToShelter(new OrganicPet("Fred", "Cat"));
+		myShelter.addPetToShelter(new OrganicPet("Lawrence", "Dog"));
+		myShelter.addPetToShelter(new OrganicPet("Matt", "Dog"));
+		printCommonAttributesOfAllPets(myShelter);
+	}
+	
 	private static String makeNCharacterStringWithDashes(String inputString, int length) {
 //		inputString.trim();
 		if(inputString.length() > length) {
@@ -58,6 +66,42 @@ public class ConsoleOutput {
 		
 		for(int i = 0; i < allAttributes.size(); i++) {
 			printSinglePetAttributes(virtualPetShelter, allAttributes.get(i).get(0));
+		}
+	}
+	private static void printCommonPetAttributesHeader() {
+		System.out.printf("|%s", make14CharacterStringWithDashes("Name"));
+		System.out.printf("|%s", make14CharacterStringWithDashes("Species"));
+		System.out.printf("|%s", make10CharacterStringWithDashes("Boredom"));
+		System.out.printf("|%s", make10CharacterStringWithDashes("BoredomCounter"));
+		System.out.printf("|%s", make10CharacterStringWithDashes("Ownership"));
+		System.out.println("|");
+		for (int i = 0; i < 2; i++) {
+			System.out.printf("|%s", make14CharacterStringWithDashes(""));
+		}
+		for (int i = 2; i < 5; i++) {
+			System.out.printf("|%s", make10CharacterStringWithDashes(""));
+		}
+		System.out.println("|");
+	}
+	private static void printCommonSinglePetAttributes(VirtualPetShelter virtualPetShelter, String name) {
+		System.out.printf("|%s", make14CharacterStringWithDashes(virtualPetShelter.getPetName(name)));
+		System.out.printf("|%s", make14CharacterStringWithDashes(virtualPetShelter.getPetSpecies(name)));
+		System.out.printf("|%s", make10CharacterStringWithDashes(Boolean.toString(virtualPetShelter.getPetBoredom(name))));
+		System.out.printf("|%s", make10CharacterStringWithDashes(Integer.toString(virtualPetShelter.getPetBoredomCounter(name))));
+		System.out.printf("|%s", make10CharacterStringWithDashes(Boolean.toString(virtualPetShelter.getPetOwnership(name))));
+		System.out.println("|");
+			
+	}
+	public static void printCommonAttributesOfSinglePet(VirtualPetShelter virtualPetShelter, String name) {
+		printCommonPetAttributesHeader();
+		printCommonSinglePetAttributes(virtualPetShelter, name);		
+	}
+	public static void printCommonAttributesOfAllPets(VirtualPetShelter virtualPetShelter) {
+		printCommonPetAttributesHeader();
+		int howManyPets = virtualPetShelter.getHowManyPets();
+		
+		for(String name : virtualPetShelter.getAllNames()) {
+			printCommonSinglePetAttributes(virtualPetShelter, name);
 		}
 	}
 }
